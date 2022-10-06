b190587ccd695725555fda07373e817ccc02f2d4
jenna-graham <jclayto2@gmail.com>
2022-10-06 11:49:49 -0700

    auth form created and rendered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,23 @@ import {
     Navigate,
   } from 'react-router-dom';
 
-//   import Dashboard from './Dashboard/Dashboard.jsx';
+  import Dashboard from './Dashboard/Dashboard.jsx';
   import Auth from './Auth/Auth.jsx';
   import UserProvider from '../state/UserContext';
+  import AuthForm from './Auth/AuthForm.jsx';
+
 
 export default function App() {
   return (
     <Router>
         <UserProvider>
         <Routes>
-            <Route index element={<Auth />}>
-            
+            <Route path="auth" element={<Auth />}>
+                <Route index element={<AuthForm mode="signin" />} />
+                <Route
+                path="signup"
+                element={<AuthForm mode="signup" />}
+                />
             </Route>
         </Routes>
         </UserProvider>
