7e5ad227c086427bc9d0d98dd05d018f97ea091e
jenna-graham <jclayto2@gmail.com>
2022-10-06 09:41:37 -0700

    user provider for auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,25 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from 'react-router-dom';
+
+//   import Dashboard from './Dashboard/Dashboard.jsx';
+  import Auth from './Auth/Auth.jsx';
+  import UserProvider from '../state/UserContext';
+
 export default function App() {
-    return <div>Hello World</div>;
-  }
+  return (
+    <Router>
+        <UserProvider>
+        <Routes>
+            <Route index element={<Auth />}>
+            
+            </Route>
+        </Routes>
+        </UserProvider>
+    </Router>
+    
+  )
+}
